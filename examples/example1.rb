9709ca621aac0df89b4d652a721476123c90d659
gnuhub <gnuhub@gmail.com>
2014-07-30 08:13:39 +0800

    增加范例准备开始调试  see #1 gnuhub/cucumber#3

---
+++ examples/example1.rb
@@ -0,0 +1,16 @@
+#!/usr/bin/env ruby
+#  ./example1.rb --help
+#  ./example1.rb -v
+require 'optparse'
+
+options = {}
+OptionParser.new do |opts|
+  opts.banner = "Usage: example.rb [options]"
+
+  opts.on("-v", "--[no-]verbose", "Run verbosely") do |v|
+    options[:verbose] = v
+  end
+end.parse!
+
+p options
+p ARGV
